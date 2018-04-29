e3582a3d3c0eaead99d22ae6e01e93fb64cb1190
kvkens <kvkens@qq.com>
2018-04-29 21:34:41 +0800

    修改uba文件正式版的前一版

--- uba.config.js
+++ uba.config.js
@@ -1,209 +1,68 @@
 /**
- * 核心配置文件
+ * Tinper-Uba 工程配置文件
  */
 
 const path = require("path");
-const hotMiddlewareScript = "webpack-hot-middleware/client?reload=true";
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-//服务启动设置
-const svrConfig = {
-  historyApiFallback: false
-};
-
-//远程代理访问，可以配置多个代理服务
-const proxyConfig = [{
-  enable: true,
-  router: "/api/*",
-  headers : {"X-XSS":"X-XSS"},
-  url: "http://cnodejs.org"
-}, {
-  enable: true,
-  router: ["/users/*", "/orgs/*"],
-  url: "https://api.github.com"
-}];
-
-
-
-//提取package里的包
-function getVendors() {
-  let pkg = require("./package.json");
-  let _vendors = [];
-  for (const key in pkg.dependencies) {
-    _vendors.push(key);
-  }
-  return _vendors;
-}
-
-
-//优化配置，对于使用CDN作为包资源的引用从外到内的配置
-const externals = {
-  "axios": "axios",
-  "react": "React",
-  "react-dom": "ReactDOM",
-  "tinper-bee": "TinperBee"
-}
-
-//默认加载扩展名、相对JS路径模块的配置
-const resolve = {
-  extensions: [
-    ".jsx", ".js", ".less", ".css", ".json"
-  ],
-  alias: {
-    components: path.resolve(__dirname, "src/components/"),
-    modules: path.resolve(__dirname, "src/modules/"),
-    routes: path.resolve(__dirname, "src/routes/"),
-    layout: path.resolve(__dirname, "src/layout/"),
-    utils: path.resolve(__dirname, "src/utils/")
-  }
-}
-
-//开发和生产需要的loader
-const rules = [{
-  test: /\.js[x]?$/,
-  exclude: /(node_modules)/,
-  include: path.resolve("src"),
-  use: [{
-    loader: "babel-loader"
-  }]
-}, {
-  test: /\.css$/,
-  use: ExtractTextPlugin.extract({
-    use: [{
-      loader: "css-loader",
-      options: {
-        modules: false
-      }
-    }, "postcss-loader"],
-    fallback: "style-loader"
-  })
-}, {
-  test: /\.less$/,
-  use: ExtractTextPlugin.extract({
-    use: [{
-      loader: "css-loader",
-      options: {
-        modules: false
-      }
-    }, 'postcss-loader', 'less-loader'],
-    fallback: 'style-loader'
-  })
-}, {
-  test: /\.(png|jpg|jpeg|gif)(\?.+)?$/,
-  exclude: /favicon\.png$/,
-  use: [{
-    loader: "url-loader",
-    options: {
-      limit: 8196,
-      name: "images/[name].[ext]"
-    }
-  }]
-}, {
-  test: /\.(eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
-  use: [{
-    loader: "file-loader",
-    options: {
-      name: "images/[name].[ext]"
-    }
-  }]
-}]
-
-
-
-//开发环境的webpack配置
-const devConfig = {
-  devtool: "cheap-module-eval-source-map",
-  entry: {
-    vendors: getVendors(),
-    app: ["./src/app.jsx", hotMiddlewareScript]
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].js",
-    publicPath: "/"
-  },
-  externals: externals,
-  module: {
-    rules: rules
-  },
-  plugins: [
-    new CommonsChunkPlugin({
-      name: "vendors"
-    }),
-    new ExtractTextPlugin({
-      filename: "[name].css"
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
+module.exports = env => {
+  let config = {
+    //入口位置和类型
+    //entry: "src/pages/*/index.js",
+    //应用类型：single=单页面、multipage=多页面
+    //appType=single，entry正常配置与webpack4一致
+    //appType=multi，entry按照blob的方式设置扫描参数
+    //https://www.npmjs.com/package/glob
+    appType: "single",
+    // appType: "multi",
+    entry: "./src/app.jsx",
+    // entry: "./src/pages/*/index.js",
+    //HTML模板设置
+    //appType=multi 会去找entry一致的路径下的html去扫描
+    html: {
       template: "./src/index.html",
-      inject: "body",
-      hash: false,
-      favicon: "./src/static/images/favicon.png",
-      chunks: ["vendors", "app"]
-    })
-  ],
-  resolve: resolve
-}
-
-
-//生产环境的webpack配置
-const prodConfig = {
-  devtool: "source-map",
-  entry: {
-    vendors: getVendors(),
-    app: "./src/app.jsx"
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].js",
-    publicPath: ""
-  },
-  externals: externals,
-  module: {
-    rules: rules
-  },
-  plugins: [
-    new CommonsChunkPlugin({
-      name: "vendors"
-    }),
-    new ExtractTextPlugin({
-      filename: "[name].css"
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    // new UglifyJSPlugin({
-    //   sourceMap : true
-    // }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    }),
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-      inject: "body",
       hash: true,
-      favicon: "./src/static/images/favicon.png",
-      chunks: ["vendors", "app"]
-    })
-  ],
-  resolve: resolve
-}
+      xhtml: true
+    },
+    resolve: {
+      //模块别名
+      //import Foo from 'components/Foo' 等价 import Foo from './src/components/Foo.jsx'
+      alias: {
+        components: path.resolve(__dirname, "src/components/"),
+        modules: path.resolve(__dirname, "src/modules/"),
+        routes: path.resolve(__dirname, "src/routes/"),
+        layout: path.resolve(__dirname, "src/layout/"),
+        utils: path.resolve(__dirname, "src/utils/"),
+        static: path.resolve(__dirname, "src/static/")
+      },
+      //默认加载ES模块扩展名
+      extensions: [
+        ".tsx"
+      ],
+    },
+    //外部全局对象导入内部对象，用于内部import { Button } from 'tinper-bee'
+    //key:value 形式，其中key是代表内部from模块名，value代表外部全局对象window.TinperBee
+    externals: {
+      // "axios": "axios",
+      // "react": "React",
+      // "react-dom": "ReactDOM",
+      // "tinper-bee": "TinperBee"
+    },
+    //加载器用于webpack处理未知文件格式
+    loader: [{
+      test: /\.ts$/,
+      use: 'ts-loader'
+    }],
+    //加载插件
+    devPlugins: [
 
-//最终向uba导出配置文件
-module.exports = {
-  devConfig,
-  prodConfig,
-  svrConfig,
-  proxyConfig
-};
+    ],
+    buildPlugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        logLevel: 'error'
+      })
+    ]
+  }
+  return config;
+}
